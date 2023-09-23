674454413acece011d8157e0cc04803ce4ad91c0
Artem Merkushyn <merkushyndev13@gmail.com>
2023-09-23 21:19:18 +0300

    implemented receiving my posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,18 +1,23 @@
 import { Router } from 'express';
 import { checkAuth } from '../utils/checkAuth.js';
-import { createPost, getAll, getById } from '../controllers/posts.js';
+import { createPost, getAll, getById, getMyPosts } from '../controllers/posts.js';
 
 const router = Router();
 
-// create http://localhost:3002/api/posts
+// create post
+// http://localhost:3002/api/posts
 router.post('/', checkAuth, createPost);
 
 // get all posts
 // http://localhost:3002/api/posts
 router.get('/', getAll);
 
-// Get Post By Id
+// get Post By Id
 // http://localhost:3002/api/posts/:id
 router.get('/:id', getById);
+
+// get my posts
+// http://localhost:3002/api/posts/user/me
+router.get('/user/me', checkAuth, getMyPosts);
 
 export default router;
