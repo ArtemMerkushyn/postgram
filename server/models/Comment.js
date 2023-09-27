09ad24cf9099c3ace875faeaae7ff6a30601147f
Artem Merkushyn <merkushyndev13@gmail.com>
2023-09-27 09:42:24 +0300

    wrote a slice to create a comment

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -7,4 +7,4 @@ const CommentSchema = new mongoose.Schema(
     },
     { timestamps: true },
 )
-export default mongoose.model('Comment', CommentSchema)
+export default mongoose.model('Comment', CommentSchema);
