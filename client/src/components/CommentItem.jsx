f0119ab3d639a827dba9e296b9257ef87c640f68
Artem Merkushyn <merkushyndev13@gmail.com>
2023-09-27 16:05:15 +0300

    wrote a component for comment

--- client/src/components/CommentItem.jsx
+++ client/src/components/CommentItem.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export const CommentItem = () => {
   return (
-    <div>CommentItem</div>
+    <div className='comment'>
+      <div className="comment__item">
+        <div className="comment__item-avatar"></div>
+      </div>
+      <div className="comment__item">
+        <div className="comment__item-username">Username</div>
+        <div className="comment__item-text"></div>
+      </div>
+    </div>
   );
 }
