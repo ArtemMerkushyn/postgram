bfc65e8b74c31a5fce1ffe922e854874fae86554
Artem Merkushyn <merkushyndev13@gmail.com>
2023-10-12 22:48:32 +0300

    implemented sorting of posts on the main page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,30 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllPosts } from '../redux/features/post/postSlice';
 import { PostsItem } from '../components/PostsItem';
 
 export const MainPage = () => {
-   const dispatch = useDispatch();
-   const { posts, popularPosts } = useSelector((state) => state.post);
+    const [sortBy, setSortBy] = useState('date'); // По умолчанию сортируем по дате
+    const dispatch = useDispatch();
+    const { posts, popularPosts } = useSelector((state) => state.post);
 
-   useEffect(() => {
-      dispatch(getAllPosts())
+    useEffect(() => {
+        dispatch(getAllPosts());
     }, [dispatch]);
 
-    if (!posts.length) {
+    const sortedPosts = sortBy === 'popular' ? popularPosts : posts;
+
+    const handleChangeSort = (e) => {
+        setSortBy(e.target.value);
+        console.log(popularPosts)
+    };
+
+    if (!sortedPosts.length) {
         return (
             <div className='text-xl text-center text-white py-10'>
                 Загрузка...
             </div>
         );
     }
-   return (
-    <div className='main-page'>
-        <div className="main-page__posts">
-            {posts?.map((post, idx) => (
-                <PostsItem key={idx} post={post}/>
-            ))}
+
+    return (
+        <div className='main-page'>
+            <div className='sort'>
+                <label htmlFor="sortBy">Сортувати по:</label>
+                <select id="sortBy" value={sortBy} onChange={handleChangeSort}>
+                    <option value="date">Даті</option>
+                    <option value="popular">Популярності</option>
+                </select>
+            </div>
+
+            <div className="main-page__posts">
+                {sortedPosts.map((post, idx) => (
+                    <PostsItem key={idx} post={post} />
+                ))}
+            </div>
         </div>
-    </div>
-   );
-}
+    );
+};
--- client/src/redux/features/post/postSlice.js
+++ client/src/redux/features/post/postSlice.js
@@ -82,7 +82,7 @@ export const postSlice = createSlice({
         [getAllPosts.fulfilled]: (state, action) => {
             state.loading = false;
             state.posts = action.payload.posts;
-            //state.popularPosts = action.payload.popularPosts;
+            state.popularPosts = action.payload.popularPosts;
         },
         [getAllPosts.rejected]: (state) => {
             state.loading = false;
